b1cc0c78b6df6ddba08ac054086c7ae488fd84de
mariemcharef <167601146+mariemcharef@users.noreply.github.com>
2024-11-14 21:42:25 +0100

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,85 +1,70 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Person;
 
-import static Person.Role.Pilote;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- *
- * @author MARIEM
- */
-public final class Pilote extends Person{
-    public Pilote(int id ,String nom ,String address ,String mail ,int phoneNumber) {
-        super(id ,nom ,address ,mail ,phoneNumber);
-        
-    }
+public sealed class Person permits Admin ,Crew ,Pilote{
+    public int id;
+    public String nom;
+    public String address;
+    public String mail;
+    public int phoneNumber;
+    public Account account;
 
+    public Person(int id, String nom, String address, String mail, int phoneNumber) {
+        this.id = id;
+        this.nom = nom;
+        this.address = address;
+        this.mail = mail;
+        this.phoneNumber = phoneNumber;
+    }
+    public Account getCompte() {
+        return account;
+    }
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getNom() {
         return nom;
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public String getMail() {
         return mail;
     }
 
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
     public int getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(int phoneNumber) {
+    public void setId(int id) {
+        this.id = id;
+    }
+     
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPhoneNum(int phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public Account getAccount() {
-        return account;
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", nom=" + nom + ", address=" + address + ", mail=" + mail + ", phoneNum=" + phoneNumber + '}';
     }
-    public boolean VerifLogin(int id, String password) {//est ce que id w password sont valides
-        return this.account.VerifLogin(id,password,Pilote);
-    }
-    public void setAccount() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter identify number: ");
-            int Id = scanner.nextInt();
-            System.out.print("Enter password: ");
-            String password = scanner.nextLine();
-            Status status=Status.ACTIVE;
-            Role role=Role.Pilote;
-            Account a=new Account(Id,password,status,role);
-            this.account=a;
-            System.out.println("Account created successfully with ID: " + id);
-        }catch (InputMismatchException e) {
-        System.out.println("Invalid input. Please enter a valid number for the ID.");
-        } catch (Exception e) {
-            System.out.println("An error occurred: " + e.getMessage());
-        }
-    }
+    
     
 }
